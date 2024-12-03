b0d01c9f1a42afc819a2406eef5cd152e25fe4a3
Andes0113 <54319516+Andes0113@users.noreply.github.com>
2024-12-03 00:00:44 -0600

    Day 2 Part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,44 +1,61 @@
-use std::fs;
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_name = if args.len() < 2 { "test.txt" } else { &args[1] };
 
-    let input = fs::read_to_string(file_name).expect("Error reading file");
+    let input = fs::read_to_string(file_name).expect("File is read");
 
     let lines = input.lines();
 
-    let num_safe = lines.map(|line| {
-        line.split(" ").map(|c| c.to_string().parse::<i32>().unwrap()).collect()
-    }).filter(|reports: &Vec<i32>| {
-        if reports.len() <= 1 {
-            return true
-        }
+    let reports: Vec<Vec<i32>> = lines
+        .map(|line| {
+            line.split(" ")
+                .map(|c| c.to_string().parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
 
-        let mut i = 1;
+    let num_safe = reports.clone().into_iter().filter(is_safe).count();
+
+    // Scuffed solution, enumerating possibilities
+    let num_safe_dampened = reports
+        .into_iter()
+        .map(|levels: Vec<i32>| {
+            for i in 0..levels.len() {
+                let mut level_subarr = levels.clone();
+                level_subarr.remove(i);
+                if is_safe(&level_subarr) {
+                    return true;
+                }
+            }
+            false
+        })
+        .filter(|cond| *cond)
+        .count();
+
+    println!("(Part 1) Safe Count: {}", num_safe);
+    println!("(Part 2) Safe Count: {}", num_safe_dampened);
+}
+
+fn is_safe(levels: &Vec<i32>) -> bool {
+    {
         let mut increasing = false;
 
-        while i < reports.len() {
-            let diff = reports[i] - reports[i - 1];
+        for i in 1..levels.len() {
+            let diff = levels[i] - levels[i - 1];
             if diff == 0 || diff.abs() > 3 {
-                return false
+                return false;
             }
             let moment_is_increase = diff > 0;
             if i == 1 {
                 increasing = moment_is_increase
             } else if moment_is_increase != increasing {
-                return false
+                return false;
             }
-            i += 1;
         }
-        
+
         true
-    }).count();
-
-
-
-    println!("(Part 1) Safe Count: {}", num_safe);
-
+    }
 }
-
